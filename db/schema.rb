b71a0a08bc9b4beacb97a7a5dc3e9d865077125c
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-11-16 17:14:47 +0900

    exec rails generate migration add_password_digest_to_users password_digest:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_073717) do
+ActiveRecord::Schema.define(version: 2020_11_16_081330) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "nickname"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
