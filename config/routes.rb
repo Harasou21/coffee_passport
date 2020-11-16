8b7835e5b935beade996c03f88ca9f5d8b8e918c
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-11-16 20:11:43 +0900

    wrote sessions/new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
+
 end
