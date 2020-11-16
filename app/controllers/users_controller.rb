a73835e18c8fe40eaeb19ceea9b70e0fa27abd1a
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-11-16 16:49:51 +0900

    write users/controller's action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
   end
 
   def create
-    
+    @user = User.find(params[:id])
+    @user.save
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
