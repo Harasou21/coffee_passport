b95b11b0ea4d3287dc82df2a6d571245c7f803c3
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-11-23 14:57:31 +0900

    create drinks_controller system

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -4,16 +4,16 @@ class DrinksController < ApplicationController
     @drinks = Drink.includes(:user).order("created_at DESC")
   end
 
+  def show
+
+  end
+
   def new
     @drink = Drink.new
   end
 
   def create
-
-  end
-
-  def show
-
+    
   end
 
   def destroy
