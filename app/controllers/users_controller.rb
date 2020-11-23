86d6e761a15dad90a4c0d0afb0df0b7117e99a09
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-11-23 14:58:00 +0900

    add destroy action to users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+  end
+
   private
     def user_params
       params.require(:user).permit(:nickname,:email,:password,:password_confirmation)
