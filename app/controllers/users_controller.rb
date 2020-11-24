b4f89b90a3472b772e6b1194d95111909fdbd973
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-11-25 06:37:25 +0900

    drinks,usersのストロングパラメーターの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:nickname,:email,:password,:password_confirmation)
+      params.require(:user).permit(:nickname,:email,:password,:password_confirmation,:image)
     end
 
     def set_user
