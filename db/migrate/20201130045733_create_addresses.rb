9205c74f640fc7218d0c1cd7983b6134edb8b0a3
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-11-30 15:06:23 +0900

    addressesテーブルのカラムを記述

--- db/migrate/20201130045733_create_addresses.rb
+++ db/migrate/20201130045733_create_addresses.rb
@@ -1,7 +1,18 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string     :fam_name        ,null: false
+      t.string     :first_name      ,null: false
+      t.string     :fam_name_kana   ,null: false
+      t.string     :first_name_kana ,null: false
+      t.date       :birthday        ,null: false
+      t.string     :postal_code     ,null: false
+      t.integer    :prefecture_id   ,null: false
+      t.string     :city            ,null: false
+      t.string     :house_num       ,null: false
+      t.string     :building_name   ,null: false
+      t.string     :phone_num       ,null: false
+      t.references :trade           ,null: false,foreign_key: true
       t.timestamps
     end
   end
