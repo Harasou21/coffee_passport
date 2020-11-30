3bc82ccb265f6b5a337a466934b40f14130779d5
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-11-30 16:23:07 +0900

    address.rbのバリデーションの記述

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,5 +1,24 @@
 class TradesController < ApplicationController
   def index
-    
+    # 商品を表示
   end
+
+  def new
+    # 商品購入ページ
+  end
+
+  def create
+    @order = TradeAddress.new(trade_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def trade_params
+      params.permit(:token, :user_id, :item_id,:fam_name,:first_name,:fam_name_kana,:first_name_kana,:birthday :postal_code, :prefecture_id, :city, :house_num, :building_name, :phone_num, :trade_id).merge(user_id: current_user.id, price: @drink.price)
+    end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,24 @@
 class Address < ApplicationRecord
   belongs_to :trade
+  with_options presence: true do
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください。' } do
+      validates :fam_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。' } do
+    validates :fam_name_kana
+    validates :first_name_kana
+    end
+
+    validates :birthday
+    validates :postal_code,format: {with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture_id
+    validates :city
+    validates :house_num
+    validates :building_name
+    validates :phone_num
+    validates :trade_id
+  end
 end
