3170122c6c53901ab2ab4cfe8126cd7d10ce4807
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-12-02 11:46:19 +0900

    trade_addressrbの作成

---
+++ app/models/trade_address.rb
@@ -0,0 +1,32 @@
+class TradeAddress
+  include ActiveModel::Model
+  # form_withやrender等の色々な機能が使える
+  attr_accessor :fam_name,:first_name,:fam_name_kana,:first_name_kana,:birthday,:postal_code, :city, :house_num, :building_name, :phone_num, :drink_id, :prefecture_id,:trade_id, :user_id, :price
+
+  with_options presence: true do
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください。' } do
+      validates :fam_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。' } do
+    validates :fam_name_kana
+    validates :first_name_kana
+    end
+
+    validates :birthday
+    validates :postal_code,format: {with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture_id, numericality: { other_than: 1 } 
+    validates :city
+    validates :house_num
+    validates :phone_num
+    validates :trade_id
+  end
+
+
+  def save
+    trade = Trade.create(user_id: user_id, drink_id: drink_id)
+    Address.create(fam_name: fam_name,first_name: first_name,fam_name_kana: fam_name_kana,postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_num: house_num, building_name: building_name, phone_num: phone_num, trade_id: trade.id)
+  end
+end
