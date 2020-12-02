c0c20ab7bb8738f41148d8005a114e059dd583d1
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-12-02 11:44:45 +0900

    tradeコントローラーの購入機能のための記述

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,24 +1,36 @@
 class TradesController < ApplicationController
+  include SessionsHelper
   def index
     # 商品を表示
+
+    @item = Drink.find(params[:drink_id])
+    @order = TradeAddress.new
+    #redirect_to root_path if @drink.trade
   end
 
   def new
+
+    @item = Drink.find(params[:drink_id])
+    @order = TradeAddress.new
     # 商品購入ページ
   end
 
   def create
-    @order = Address.new(trade_params)
+    
+    @item = Drink.find(params[:drink_id])
+    @order = TradeAddress.new(trade_params)
+    
+   # binding.pry
     if @order.valid?
       @order.save
       redirect_to root_path
     else
-      render 'new'
+      render 'index'
     end
   end
 
   private
     def trade_params
-      params.permit(:token, :user_id, :item_id,:fam_name,:first_name,:fam_name_kana,:first_name_kana,:birthday :postal_code, :prefecture_id, :city, :house_num, :building_name, :phone_num, :trade_id).merge(user_id: current_user.id, price: @drink.price)
+      params.permit(:drink_id,:user_id,:fam_name,:first_name,:fam_name_kana,:first_name_kana,:birthday, :postal_code, :prefecture_id, :city, :house_num, :building_name, :phone_num, :trade_id).merge(user_id: current_user.id,price: @item.price)
     end
 end
