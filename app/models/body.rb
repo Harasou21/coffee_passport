5c2658c89af644d214459149a919f2627250fd79
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-12-09 20:42:27 +0900

    コーヒーに関するカテゴリーのデータ設定

--- app/models/body.rb
+++ app/models/body.rb
@@ -1,2 +1,8 @@
-class Body < ApplicationRecord
+class Body < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'LIGHT(軽い)' },
+    { id: 3, name: 'MEDIUM(ほどよい)' },
+    { id: 4, name: 'FULL(しっかり)' }
+  ]
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,2 +1,9 @@
-class Region < ApplicationRecord
+class Region < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'マルチリージョン' },
+    { id: 3, name: 'ラテンアメリカ' },
+    { id: 4, name: 'アフリカ' },
+    { id: 5, name: 'アジア、太平洋' }
+  ]
 end
