69ac0c378e022f2e00c020e6a0a272def85ae42f
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-12-09 20:42:27 +0900

    コーヒーに関するカテゴリーのデータ設定

--- app/models/acidity.rb
+++ app/models/acidity.rb
@@ -1,2 +1,8 @@
-class Acidity < ApplicationRecord
+class Acidity < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'LOW(少ない)' },
+    { id: 3, name: 'MEDIUM(ほどよい)' },
+    { id: 4, name: 'HIGH(強い)' }
+  ]
 end
