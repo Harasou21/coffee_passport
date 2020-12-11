710e89f4718da7e7afb97288f590932a11e87424
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-12-11 20:46:03 +0900

    ドリンクのactive hashカテゴリー実装時にgit reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_091200) do
+ActiveRecord::Schema.define(version: 2020_12_11_113815) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
