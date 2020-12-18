236d5d470f92b309148b534e87d8e27792c708e2
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-12-18 15:56:56 +0900

    検索機能実装のため、ダミーカテゴリーの削除

--- app/models/processing.rb
+++ app/models/processing.rb
@@ -1,6 +1,5 @@
 class Processing < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
     { id: 2, name: 'WASHED(水洗式)' },
     { id: 3, name: 'SEMI-WASHED(半水洗式)' },
     { id: 4, name: 'NATURAL(乾燥式)' },
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,6 +1,5 @@
 class Region < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
     { id: 2, name: 'マルチリージョン' },
     { id: 3, name: 'ラテンアメリカ' },
     { id: 4, name: 'アフリカ' },
