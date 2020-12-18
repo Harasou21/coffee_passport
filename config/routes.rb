03d991db1c377a01f86ac0f4f11973e24865e9cb
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-12-18 20:20:12 +0900

    commentのルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,11 @@ Rails.application.routes.draw do
     collection do
       get 'search'
     end
+
+    resources :comments,only: :create
+    # あるツイートに対してのコメントと言う
+    # 親子関係を表現したパスが、コメント投稿に必要なリクエストの
+    # パスになります
     
     resources :trades,only: [:index,:new,:create]
   end
