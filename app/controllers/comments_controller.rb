919f8a735cbfd50d794e756ea80b9c351ea59038
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-12-18 20:31:17 +0900

    comment_controllerの処理を記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  include SessionsHelper
+  def create
+    Comments.create(comment_params)
+    redirect_to "/drinks/#{comment.drink.id}"
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:text).merge(user_id: current_user.id,drink_id: params[:drink_id])
+    end
 end
