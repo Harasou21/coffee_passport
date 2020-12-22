5b05127f365f654166ddf995b92a83b3b1014f66
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-12-22 16:28:27 +0900

    非同期でいいね、いいね解除するための準備

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,22 @@
 class LikesController < ApplicationController
   include SessionsHelper
+
+  before_action :drink_params
   def create
     Like.create(user_id: current_user.id, drink_id: params[:id])
-    redirect_to drinks_path
+    #redirect_to drinks_path
+    # jsを用いるので画面遷移は行わない
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, drink_id: params[:id]).destroy
-    redirect_to drinks_path
+
   end
 
+  private
+
+    def drink_params
+      @drink = Drink.find(params[:id])
+    end
 
 end
