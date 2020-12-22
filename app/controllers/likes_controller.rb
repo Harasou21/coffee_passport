afe77b7722a2578f386a03ff328f668bb7d50aaf
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-12-22 15:31:03 +0900

    likes_controllerにcreateアクションを記述

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,8 @@
 class LikesController < ApplicationController
+  include SessionsHelper
+  def create
+    Like.create(user_id: current_user.id, drink_id: params[:id])
+    redirect_to drinks_path
+  end
+
 end
