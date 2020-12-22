e9727281305a04ab474a8b68a13a61f28e39f8c9
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-12-22 15:15:07 +0900

    rateモデル、コントローラー、テーブル削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_18_110856) do
+ActiveRecord::Schema.define(version: 2020_12_22_061401) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
