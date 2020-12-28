fa4c7ebc2e5533509e3fddea64034a85e73a98bf
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-12-28 16:30:10 +0900

    動的なタイトル変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
-
+  def full_title(page_title = '')
+    base_title = "Coffee Passport"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
+
