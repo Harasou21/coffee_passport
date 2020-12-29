4d2eaabd8ef3b25cd62d762543074ad51bc2aee1
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-12-29 13:36:58 +0900

    購入ページの実装

--- app/controllers/sold_drinks_controller.rb
+++ app/controllers/sold_drinks_controller.rb
@@ -1,7 +1,10 @@
 class SoldDrinksController < ApplicationController
   
   def index
-    @admin_user = User.find(id: 6)
+    @admin_user = User.find_by(id: 6)
     @drinks = @admin_user.drinks
   end
+
+
+
 end
