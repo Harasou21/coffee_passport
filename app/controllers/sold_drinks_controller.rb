1001aa7195bfd9614de81b5162ffd1b3fe4a5909
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2021-01-18 15:37:36 +0900

    様々なレイアウトを整え

--- app/controllers/sold_drinks_controller.rb
+++ app/controllers/sold_drinks_controller.rb
@@ -2,7 +2,8 @@ class SoldDrinksController < ApplicationController
   
   def index
     @admin_user = User.find_by(id: 6)
-    @drinks = @admin_user.drinks
+    @drinks = @admin_user.drinks.paginate(page: params[:page],per_page: 10)
+    @title = "商品を購入する"
   end
 
 
