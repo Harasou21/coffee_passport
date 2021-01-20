b0fa7c615b1c7d9938ae7f32e92df65795723874
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2021-01-20 14:37:08 +0900

    不要な記述を削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,7 @@ require("./tag")
 require("../slide")
 require('./preview') 
 //= require rails-ujs
-//= require jquery_ujs
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
