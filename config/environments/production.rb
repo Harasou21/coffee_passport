8cf23e00a59927a40ca00204bc4aec623a24d85f
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2021-01-21 12:22:03 +0900

    本番環境のメイラーの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  host = 'http://54.178.31.60/'
+  Rails.application.routes.default_url_options[:host] = host
   # Code is not reloaded between requests.
   config.cache_classes = true
 
