b6ee9993aad1835825b7450ab51a9834c8c9b608
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2021-01-21 11:18:26 +0900

    selfを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -151,8 +151,8 @@ class User < ApplicationRecord
     end
 
     def activate
-      update_attribute(:activated,    true)
-      update_attribute(:activated_at, Time.zone.now)
+      self.update_attribute(:activated,    true)
+      self.update_attribute(:activated_at, Time.zone.now)
     end
   
     # 有効化用のメールを送信する
