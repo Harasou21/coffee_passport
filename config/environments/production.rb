e5714d7fb8af2500d5794f2054eed619845c8d88
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2021-01-27 21:52:40 +0900

    間違えた

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     #enable: true,
-    address: 'smtp.sendgrid.com',
+    address: 'smtp.sendgrid.net',
     port: 587,
     domain: 'gmail.com',
     authentication: :plain,
