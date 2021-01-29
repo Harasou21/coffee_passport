51fb2664553d9a21d5f8c893314177dbd60d2140
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2021-01-30 02:46:18 +0900

    ユーザーにメールを送る処理を記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # @user.send_activation_email
+      @user.send_activation_email
       flash[:info] = "メールをチェックしてアカウントを有効化してください."
       
       redirect_to root_url
