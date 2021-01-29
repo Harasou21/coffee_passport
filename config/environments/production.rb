96a95e2000ecc586bfd25da902d98997f2f2259e
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2021-01-30 02:32:56 +0900

    本番環境のメイラーの設定をsendgridに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,14 +5,14 @@ Rails.application.configure do
   Rails.application.routes.default_url_options = {host: host,protocol: 'http'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     #enable: true,
-    address: 'smtp.gmail.com',
+    address: 'smtp.sendgrid.net',
     port: 587,
     domain: 'gmail.com',
     authentication: :plain,
-    user_name: ENV['GOOGLE_USER_NAME'],
-    password: ENV['GOOGLE_PASSWORD'],
+    user_name: "apikey",
+    password: ENV['SENDGRID_PASSWORD'],
     enable_starttls_auto:  true
     #openssl_verify_mode: 'peer',
     #ssl: 465,
