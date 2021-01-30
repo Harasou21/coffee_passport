a0995f430878bdf37fe6c2359448a0b2be099e51
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2021-01-30 22:10:41 +0900

    ハウスブレンドのサンプルデータを投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 出品ユーザーを定義
+User.create(
+  id: 6,
+  nickname: "出品ユーザー",
+  email: "harasou21soccer@gmail.com",
+  password: ENV['ADMIN_USER_PASSWORD'],
+  password_confirmation: ENV['ADMIN_USER_PASSWORD'],
+  activated: true,
+  activated_at: "Time.zone.now"
+)
+
+ houseblend = Drink.create(
+  name: "ハウスブレンド",
+  price: 1050,
+  explain: " #RICH #LIVELY ナッツやココアの味わいが特徴で、スターバックス ローストによって引き出された香りやコクなど、すべてのバランスがよいコーヒー。",
+  user_id: 6,
+  region_id: 3,
+  body_id: 3,
+  acidity_id: 3,
+  processing_id: 2
+)
+
+houseblend.image.attach(io: File.open(Rails.root.join("app/assets/images/houseblend.jpeg")),filename: "houseblend.jpeg")
