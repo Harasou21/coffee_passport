723c874c3a8b8087394619f0972373535750a40c
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2021-02-02 01:47:59 +0900

    サンプルデータの投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # 出品ユーザーを定義
-User.create(
+User.create!(
   id: 6,
   nickname: "出品ユーザー",
   email: "harasou21soccer@gmail.com",
@@ -17,7 +17,7 @@ User.create(
   activated_at: "Time.zone.now"
 )
 
- houseblend = Drink.create(
+houseblend = Drink.create!(
   name: "ハウスブレンド",
   price: 1050,
   explain: " #RICH #LIVELY ナッツやココアの味わいが特徴で、スターバックス ローストによって引き出された香りやコクなど、すべてのバランスがよいコーヒー。",
@@ -25,7 +25,36 @@ User.create(
   region_id: 3,
   body_id: 3,
   acidity_id: 3,
-  processing_id: 2
+  processing_id: 2,
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("app/assets/images/houseblend.jpeg"),filename: "houseblend.jpeg")
+ 
 )
 
-houseblend.image.attach(io: File.open(Rails.root.join("app/assets/images/houseblend.jpeg")),filename: "houseblend.jpeg")
+#houseblend.image.attach(io: File.open(Rails.root.join("app/assets/images/houseblend.jpeg")),filename: "houseblend.jpeg")
+
+
+
+# 複数の書き方はこんな感じ
+
+# Note.create!(
+#   [
+#     {
+#        title: "OS",
+#        user_id: 1,
+#        category_id: 1,
+#        explanation:
+#         "OSとはOperation System（オペレーティング・システム）の略で、アプリやデバイスを動作させるための基本となるソフトウェアのことです。 具体的には、キーボードやマウス・タッチパッドなどのデバイスから入力した情報をアプリケーションに伝え、またソフトウェアとハードウェアの連携を司る中枢的な役割を果たします。 パソコンやスマートフォンにはゲームやワープロ・表計算などさまざまな仕事をするアプリケーションがありますが、それらはOSごとに開発されるのが通常です。\r\n\r\n\r\n```\r\n例）\r\n・Mac OS\r\n・Window OS\r\n・Linux\r\n```\r\n\r\n",
+#        rate: 3,
+#     },
+#     {
+#        title: "UI(ユーザーインターフェース)",
+#        user_id: 1,
+#        category_id: 1,
+#        explanation:
+#         "UIとは、ユーザーインターフェイス（User Interface）の略称で、一般的にユーザー（利用者）と製品やサービスとのインターフェース（接点）すべてのことを意味します。\r\n\r\nユーザーとは、主にWebサービス（サイト）・アプリケーション・ソフトウェアを利用する人を言います。\r\nインターフェースとは、直訳で接点やつながりを意味し、何か2つのものの間での伝達方法や接続の仕方のことを言います。\r\n\r\nWebサイトでいうところのUIは、サイトの見た目や、使いやすさのことを指します。よくUI＝見た目、と勘違いをしている方もいるかと思いますが、レイアウトや使用されている画像はもちろん、文字のフォント、メニューやボタンの操作性などユーザーが目にするもの・操作するものすべてが含まれています\r\n\r\n```\r\n・ 画面\r\n・ 見た目\r\n・ 使い勝手\r\n```",
+#        rate: 3,
+#     }
+
+
+#   ]
+# )
