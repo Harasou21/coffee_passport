e6f9a77f92daa138551aed2b981850dfd17d6252
harasou21 <harasou41soccer@gmail.com>
2021-03-14 14:13:28 +0900

    メイラー の設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,17 +76,19 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    #enable: true,
+  #  #enable: true,
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
     authentication: :login,
-    user_name: ENV["GOOGLE_USER_NAME"],
-    password: ENV["GOOGLE_PASSWORD"],
+    user_name: ENV['GOOGLE_USER_NAME'],
+    password: ENV['GOOGLE_PASSWORD'],
     enable_starttls_auto:  true,
-    #openssl_verify_mode: 'peer',
+     #openssl_verify_mode: 'peer',
     ssl:  false,
     tls: false
 
   }
+
+  
 end
