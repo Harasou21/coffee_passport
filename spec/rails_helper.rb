464294795224b6e1c91400bbcf1810b1183d7fb4
harasou21 <harasou41soccer@gmail.com>
2021-04-13 22:13:37 +0900

    rspecのmailerテスト機能を使うため

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'email_spec'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
@@ -61,4 +62,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include(EmailSpec::Helpers)
+  config.include(EmailSpec::Matchers)
 end
