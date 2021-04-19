66cf45fa266db6393949c4d8facf27403f019459
harasou21 <harasou41soccer@gmail.com>
2021-04-20 01:53:56 +0900

    factory :sold_drink ,class: Drink doを記述

--- spec/factories/drinks.rb
+++ spec/factories/drinks.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :drink do
+    # class: モデル名 ってやる必要あるが、キー名で推測してるらしい
     name                {"TOKYOロースト"}
     price               {350}
     explain             {"これはコーヒーの説明です"}
@@ -8,6 +9,27 @@ FactoryBot.define do
     acidity_id          {Acidity.all.sample}
     processing_id       {Processing.all.sample}
     likes_count         {2}
+    association :user 
+    association :region
+    association :body
+    association :acidity
+    association :processing
+
+    after(:build) do |drink|
+      drink.image.attach(io: File.open('app/assets/images/ethiopia.jpg'), filename: 'ethiopia.jpg')
+    end
+  end
+
+  factory :sold_drink ,class: Drink do
+    name                {"TOKYOロースト"}
+    price               {350}
+    explain             {"これはコーヒーの説明です"}
+    region_id           {2}
+    body_id             {Body.all.sample}
+    acidity_id          {Acidity.all.sample}
+    processing_id       {Processing.all.sample}
+    likes_count         {2}
+    user_id             {6}
     association :user 
     association :region
     association :body
