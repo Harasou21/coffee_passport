a449660f5dba2d1967fbbd179f2abb64aac2a6df
harasou21 <harasou41soccer@gmail.com>
2021-05-15 12:44:22 +0900

    unicornの設定変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,10 @@
-app_path = File.expand_path('../../', __FILE__)  # 「../」が一つ増えている
+app_path = File.expand_path('../../../', __FILE__)  # 「../」が一つ増えている
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory "#{app_path}/current"  # 「current」を指定
+working_directory app_path  # 「current」を指定
 
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"  # 「shared」の中を参照するよう変更
