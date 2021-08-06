6c073226975ff8c1f963c7e0db62d21c4bf8e5da
harasou21 <harasou41soccer@gmail.com>
2021-08-07 01:04:11 +0900

    vueをフロントにするので、pagy削除

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @pagy,@drinks = pagy(@user.drinks.order('created_at DESC').includes(image_attachment: :blob))
+    @drinks = @user.drinks.order('created_at DESC').includes(image_attachment: :blob)
   end
 end
