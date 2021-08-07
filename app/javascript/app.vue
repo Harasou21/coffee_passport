9146b690f51e7694125907abde3117d6391b1d82
harasou21 <harasou41soccer@gmail.com>
2021-08-07 15:49:11 +0900

    railsからjsonデータを取得する処理

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,11 +1,31 @@
 <template>
-  
+<ul>
+  <li v-for="drink in drinks" :key="drink.id">
+    {{drink.name}}
+    {{drink.price}}
+  </li>
+</ul>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  setup() {
-    
+  data: function (){
+    return{
+      drinks: "drinks"
+    }
   },
+  mounted () {
+    this.setDrink();
+  },
+  methods: {
+    setDrink: function(){
+      axios.get('/api' + location.pathname)
+        .then(response => (
+          this.drinks = response.data
+        ))
+    }
+  }
 }
 </script>
