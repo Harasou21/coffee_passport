810c9e8a7ee1920e91f7e5fe71669d9db06dd138
harasou21 <harasou41soccer@gmail.com>
2021-08-15 15:04:36 +0900

    api/likes_controllerの定義

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,10 +1,26 @@
 class Api::LikesController < ApplicationController
-  def index
+  include SessionsHelper
+
+  before_action :set_variables
+
+  def like
+    @like = current_user.likes.new(drink_id: @drink.id)
+    @like.save
+    # redirect_to drinks_path
+    # jsを用いるので画面遷移は行わない
+    # binding.pry
+    #=> like.js.erbに遷移する。
   end
 
-  def create
+  def unlike
+     @like = current_user.likes.find_by(drink_id: @drink.id)
+     @like.destroy
+    # binding.pry
   end
 
-  def destroy
+  private
+
+  def set_variables
+    @drink = Drink.find(params[:drink_id])
   end
 end
