0b57d1edd3e700da5dcb4cd97c8d75c9a1e8e077
harasou21 <harasou41soccer@gmail.com>
2021-08-22 22:36:19 +0900

    api::likes_controllerの再定義

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,31 +1,30 @@
 class Api::LikesController < ApplicationController
-  include SessionsHelper
 
   before_action :set_variables
 
   def index
     # その投稿のいいね一覧を取得
-    @like = Like.filter_by_drink(params[:post_id]).select(:id, :user_id, :drink_id)
+    render json: Like.filter_by_drink(params[:drink_id]).select(:id, :user_id, :drink_id)
   end
 
-  def like
-    @like = current_user.likes.new(drink_id: @drink.id)
+  def create
+    @like = @user.likes.new(drink_id: @drink.id)
     @like.save
-    # redirect_to drinks_path
-    # jsを用いるので画面遷移は行わない
-    # binding.pry
-    #=> like.js.erbに遷移する。
+
+    head :created
   end
 
-  def unlike
-     @like = current_user.likes.find_by(drink_id: @drink.id)
+  def destroy
+     @like = @user.likes.find_by(drink_id: @drink.id)
      @like.destroy
-    # binding.pry
+    head :ok
   end
 
   private
 
   def set_variables
+
+    @user = User.find(params[:user_id])
     @drink = Drink.find(params[:drink_id])
   end
 
