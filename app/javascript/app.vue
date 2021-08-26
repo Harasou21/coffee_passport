19cdfd78e15490d152294d39c959e983d5f5a9fa
harasou21 <harasou41soccer@gmail.com>
2021-08-26 23:43:24 +0900

    モーダルウィンドウを表示する記述

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -3,12 +3,13 @@
  
   <div class='item-contents'>
    
-        <h2 class='title'>タイムライン</h2>
+    <h2 class='title'>タイムライン</h2>
     <ul class='item-lists'>
         <li v-for="drink in drinks" :key="drink.id" class="list" >
               <div class="user-info-timeline">
+                  <img class="user-img-timeline" v-bind:src="drink.user_img" > 
                   <div class="username-timeline">
-                    {{drink.nickname}}
+                      {{drink.nickname}}
                   </div>
               </div>
 
@@ -24,9 +25,6 @@
               </div>
               <div class='item-explain'>
                 {{drink.explain}}
-                {{drink.region_id}}
-                {{drink.body_id}}
-                {{ drink.acidity_id}}
               </div>
               <div class='item-tag'>
 
@@ -34,9 +32,14 @@
               <div class="like-and-comment">
                  <likeButton :drinkId=drink.id></likeButton>
                  <i class="far fa-comment fa-lg"></i>
+                <button v-on:click="show(drink)">詳細を表示</button>
               </div>
             </div>
-
+                <modal v-bind:name="'display-drink-'+drink.id"
+                 height="1000px" 
+                 styles="background-color: bisque">
+                  <drinkShow :drink=drink></drinkShow>
+                </modal>
             <div v-if="drink.body_id === 2" class="body-light"></div>
             <div v-else-if="drink.body_id === 3" class="body-medium"></div>
             <div v-else-if="drink.body_id === 4" class="body-full"></div>
@@ -48,24 +51,24 @@
             <div v-else class="acidity-nothing"></div>
         </li>
       </ul> 
+
       </div>
+
+
 </div>
-
-
-
-
-
-
 </template>
 
 <script>
- import axios from 'axios';
-import likeButton from './packs/components/like/likeButton.vue'
+
+import axios from 'axios';
+import likeButton from './packs/components/like/likeButton.vue';
+import drinkShow from './packs/components/drinks/show.vue';
 
 
 export default {
   components: {
-      likeButton
+      likeButton,
+      drinkShow
   },
   data: function(){
     return {
@@ -81,6 +84,12 @@ export default {
       .then(response =>(
         this.drinks = response.data
       ))
+    },
+    show : function(drink) {
+        this.$modal.show(`display-drink-${drink.id}`);
+    },
+    hide : function () {
+      this.$modal.hide('display-drink-show');
     }
   }
 }
