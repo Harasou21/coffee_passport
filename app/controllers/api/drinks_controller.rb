ede1ca9648d173b6c922587356efca2980719587
harasou21 <harasou41soccer@gmail.com>
2021-08-31 17:21:59 +0900

    きれいなコードに変更

--- app/controllers/api/drinks_controller.rb
+++ app/controllers/api/drinks_controller.rb
@@ -7,12 +7,11 @@ class Api::DrinksController < ApplicationController
       @user = current_user
   
       following_ids = 'SELECT followed_id FROM relationships WHERE follower_id = :user_id'
-  
+      
       @drinks = Drink.includes(:user ,{image_attachment: :blob})
-                      .where.not(user_id: 6)
-                      .where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: @user.id)
-                      .order('drinks.created_at DESC')
-    
+                     .where.not(user_id: 6)
+                     .where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: @user.id)
+                     .order('drinks.created_at DESC')
     end
   end
 
