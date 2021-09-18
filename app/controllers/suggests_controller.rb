aa77f0bb8ba3a3677753f0c6f0a190bfaa3ffb77
harasou21 <harasou41soccer@gmail.com>
2021-09-18 15:45:18 +0900

    rails generate controller suggestsを実行

---
+++ app/controllers/suggests_controller.rb
@@ -0,0 +1,2 @@
+class SuggestsController < ApplicationController
+end
---
+++ spec/requests/suggests_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "Suggests", type: :request do
+  describe "GET /index" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end
