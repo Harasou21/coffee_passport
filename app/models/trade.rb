295956d1b6aa9361e7436339835471b09f140dda
harasou21 <harasou41soccer@gmail.com>
2021-10-04 12:23:52 +0900

    trades_countのアソシエーションを組んだ

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -2,6 +2,6 @@ class Trade < ApplicationRecord
   include SessionsHelper
   belongs_to :user
   belongs_to :drink
-  belongs_to :drink, counter_cache: :likes_count
+  belongs_to :drink, counter_cache: :trades_count
   has_one :address
 end
